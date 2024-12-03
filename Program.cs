865eeff6410c634b84144c74aebca9196bb64884
Alejandro Mariscal Romero <87366244+mariscalromeroalejandro@users.noreply.github.com>
2024-12-03 17:35:38 +0100

    dateTime

--- Program.cs
+++ Program.cs
@@ -11,7 +11,26 @@ namespace HelloWorld
             // Exercise1_FacebookLikes.Run();
             // Exercise2_RevertName.Run();
             // Exercise3_UniqueNumbers.Run();
-            Exercise4_3Smallest.Run();
+            // Exercise4_3Smallest.Run();
+
+            var dateTime = new DateTime(2015, 1, 1);
+            var now = DateTime.Now;
+            var today = DateTime.Today;
+
+            Console.WriteLine($"Hour: {now.Hour}");
+            Console.WriteLine($"Minute: {now.Minute}");
+            Console.WriteLine($"Second: {now.Second}");
+
+            var tomorrow = now.AddDays(1);
+            var yesterday = now.AddDays(-1);
+            
+            
+            Console.WriteLine(now.ToLongDateString());
+            Console.WriteLine(now.ToShortDateString());
+            Console.WriteLine(now.ToLongTimeString());
+            Console.WriteLine(now.ToShortTimeString());
+            Console.WriteLine(now.ToString("yyyy-MM-dd HH:mm:ss"));
+
 
         }
     }
--- obj/Debug/net9.0/HelloWorld.AssemblyInfo.cs
+++ obj/Debug/net9.0/HelloWorld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d6cebf81e4bc67a4bb4857e7cf0fcc6bbd289a43")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6b28054459ab529e77b76b5bf8e74554cbe892d3")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
