dcf0055e84a337f6d1c88971a283c7e31d446638
Alejandro Mariscal Romero <87366244+mariscalromeroalejandro@users.noreply.github.com>
2024-12-11 15:55:10 +0100

    procedural programming

--- Program.cs
+++ Program.cs
@@ -7,8 +7,22 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Exercises.RunExercises();
+            // Exercises.RunExercises();
             
+            // Procedural programming
+            Console.WriteLine("What's your name?");
+            var name = Console.ReadLine();
+            Console.WriteLine("Reversed name: " + ReverseName(name));
+        }
+
+        public static string ReverseName(string name)
+        {
+            var array = new char[name.Length];
+            for (var i = name.Length; i > 0; i--) 
+                array[name.Length - i] = name[i - 1];
+            var reversed = new string(array);
+            
+            return reversed;
         }
     }
 }
